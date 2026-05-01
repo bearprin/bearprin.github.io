c4357d54f9e7a3bf5496d62a1c60a9b6aaa47b76
bearprin <zixiong_wang@outlook.com>
2026-05-01 11:48:47 +0000

    Deploying to gh-pages from @ bearprin/bearprin.github.io@6c009f9c7fb52511d1bf590e1f1edc2acaee1932 🚀

---
+++ assets/js/google-analytics-setup.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-L3V3YFHY81");
---
+++ assets/js/search-data.js
@@ -0,0 +1,84 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "news-two-papers-accepted-by-siggraph-asia-2023",
+          title: 'Two papers accepted by SIGGRAPH Asia 2023.',
+          description: "",
+          section: "News",},{id: "news-started-ph-d-journey-in-prof-beibei-wang-s-render-group-looking-forward-to-it",
+          title: 'Started Ph.D. journey in Prof. Beibei Wang’s render group. Looking forward to it!...',
+          description: "",
+          section: "News",},{id: "news-one-paper-accepted-by-neurips-2023",
+          title: 'One paper accepted by NeurIPS 2023.',
+          description: "",
+          section: "News",},{id: "news-fabricgen-accepted-by-cvpr-2026",
+          title: 'FabricGen accepted by CVPR 2026.',
+          description: "",
+          section: "News",},{id: "news-himat-accepted-by-eurographics-2026-cgf",
+          title: 'HiMat accepted by Eurographics 2026 (CGF).',
+          description: "",
+          section: "News",},{id: "news-puresample-accepted-by-siggraph-2026-conference-track",
+          title: 'PureSample accepted by SIGGRAPH 2026 (Conference Track).',
+          description: "",
+          section: "News",},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%7A%69%78%69%6F%6E%67_%77%61%6E%67@%6F%75%74%6C%6F%6F%6B.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/bearprin", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=DqEP_nIAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
